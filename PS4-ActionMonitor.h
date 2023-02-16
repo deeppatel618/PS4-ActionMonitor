37c08818a9bab47656ccaa76bb370bc3232748cf
BlackCommando2 <75882787+BlackCommando2@users.noreply.github.com>
2023-02-17 00:16:06 +0530

    Add files via upload

--- PS4-ActionMonitor.h
+++ PS4-ActionMonitor.h
@@ -2,11 +2,11 @@
 #include "Updater/Updater.h"
 void none(){};
 void none(int v){};
+void none(int lx,int ly,int rx,int ry,int r){};
 void none(int x,int y,int r){};
 #include "Button/Button.h"
-#include "Movement/Movement.h"
 #include "AllListner/AllListner.h"
-
+AllListner *listner;
 void updateAllEntities()
 {
     Ps4.update();
@@ -16,4 +16,9 @@ void initPS4(String addr)
 {
     PS4.begin(addr.c_str());
     PS4.attach(updateAllEntities);
+}
+
+void getAllAnalog(bool setAllAnalog = false)
+{
+    listner = new AllListner(setAllAnalog);
 }
